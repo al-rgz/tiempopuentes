6730fdd0905a103f3d6aaa62d1f116da3cbb75f5
al-rgz <al_mucart@live.com.mx>
2026-06-14 20:28:03 -0600

    Lo mismoe del cmit anterior v2

--- puentes/puentes.js
+++ puentes/puentes.js
@@ -126,8 +126,8 @@ const main = document.getElementById('container');
                  <p class='boton_cerrar_display' id='boton_cerrar_display'>X</p>
 
             </div> 
-               <iframe width="390" height="315" src="${videoSur}"> </iframe>
-               <iframe width="390" height="315" src="${videoNorte}"> </iframe>
+               <iframe width="395" height="315" src="${videoSur}"> </iframe>
+               <iframe width="395" height="315" src="${videoNorte}"> </iframe>
                
                
                `;
