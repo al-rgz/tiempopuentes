1ed0cad83cff9a1dd93ed18873b85ef89d832d24
al-rgz <al_mucart@live.com.mx>
2026-07-18 23:55:40 -0600

    Reversed previous commit

--- puentes/puentes.js
+++ puentes/puentes.js
@@ -148,7 +148,7 @@ const main = document.getElementById('container');
                <ul class= "lista_precios" id="lista_precios">              
 
                 <li class="">
-                 <h4>Precios de cruce</h4>
+                 <h3>Precios de cruce</h3>
                  <img class="flecha" id="flecha" src="/assets/arrow-right.png">
 
                 <span class ="hidden_precio">
